07a42a37e39b48aa7ab7069b64bdf0245fa928df
Nathan Lebas <nathanlebas3@gmail.com>
2018-05-18 11:35:06 +0200

    Prepare for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-  resources :articles
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/cocktails', to: "cocktails#index"
-  get '/cocktail/:id', to: "cocktails#show", as: :cocktail
-  get '/cocktails/new', to: "cocktails#new", as: :new_cocktail
-  post '/cocktails', to: "cocktails#create"
 
-  get '/cocktails/doses/new', to: "doses#new", as: :dose
-  #post '/cocktails/:id/doses/create', to: "doses#create", as: :add
-  #delete "/doses/:id" to: "doses#destroy", as: :destroy
+  resources :cocktails do
+    resources :doses
+  end
+
 end
